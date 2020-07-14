726e86299d43e7b6138093ae25774c3c57c4a480
Andriy Yurchuk <paltashka50@gmail.com>
2020-07-14 13:13:33 +0300

    +

--- js/country.js
+++ js/country.js
@@ -1,33 +1,3 @@
-function handlePhoneInput() {
-  $(document).ready(function () {
-    $("#phone").on("input", function (event) {
-      const inputValue = event.target.value;
-      if (inputValue === "" || inputValue === "+") {
-        $("#country-flag")[0].src = "";
-      }
-      const datalist = $("datalist");
-      const options = datalist[0].options;
-      let code;
-      for (let i = 0; i < options.length; i++) {
-        if (inputValue === options[i].value) {
-          // if (inputValue === '+1') {
-          //   console.log(options[i].dataset.id)
-          //   if (options[i].dataset.id === '1')
-          //     code = 'us.png';
-          //     break;
-          //   } else {
-          //     code = 'cn.png';
-
-          //   }
-          code = options[i].dataset.countryCode.toLowerCase();
-          $("#country-flag")[0].src = `/images/flags/${code}.png`;
-          break;
-        }
-      }
-    });
-  });
-}
-
 $(document).ready(function () {
   // $("#phone").intlTelInput({
   //   utilsScript: "js/utils.js",
