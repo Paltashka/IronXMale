4b331bfc49ef1e3eb328ef07b4bd6b1b64118320
Andriy Yurchuk <paltashka50@gmail.com>
2020-07-14 12:55:59 +0300

    +

--- js/country.js
+++ js/country.js
@@ -2,20 +2,36 @@ function handlePhoneInput() {
   $(document).ready(function () {
     $("#phone").on("input", function (event) {
       const inputValue = event.target.value;
-      
-      if (inputValue === '' || inputValue === '+') {
-        $('#country-flag')[0].src = '';
+      if (inputValue === "" || inputValue === "+") {
+        $("#country-flag")[0].src = "";
       }
-      const datalist = $('datalist');
+      const datalist = $("datalist");
       const options = datalist[0].options;
+      let code;
       for (let i = 0; i < options.length; i++) {
         if (inputValue === options[i].value) {
-          const code = options[i].dataset.countryCode.toLowerCase();
-          console.log(code);
-          $('#country-flag')[0].src = `/images/flags/${code}.png`;
+          // if (inputValue === '+1') {
+          //   console.log(options[i].dataset.id)
+          //   if (options[i].dataset.id === '1')
+          //     code = 'us.png';
+          //     break;
+          //   } else {
+          //     code = 'cn.png';
+
+          //   }
+          code = options[i].dataset.countryCode.toLowerCase();
+          $("#country-flag")[0].src = `/images/flags/${code}.png`;
           break;
         }
       }
     });
   });
 }
+
+$(document).ready(function () {
+  // $("#phone").intlTelInput({
+  //   utilsScript: "js/utils.js",
+  // });
+  var input = document.querySelector("#phone");
+  window.intlTelInput(input);
+});
