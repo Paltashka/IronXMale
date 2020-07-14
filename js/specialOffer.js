47e17c75fa9747db4128f4f75231e83e82235c92
Andriy Yurchuk <paltashka50@gmail.com>
2020-07-14 17:26:43 +0300

    +

--- js/specialOffer.js
+++ js/specialOffer.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-  // $("#phone").intlTelInput({
-  //   utilsScript: "js/utils.js",
-  // });
   var input = document.querySelector("#phone");
   window.intlTelInput(input);
+
+  
 });
